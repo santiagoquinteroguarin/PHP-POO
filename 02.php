69ebde9dee0330276fa300e9b34bf07316815e0c
Santiago Quintero <quinterosantiago15@gmail.com>
2021-01-29 10:11:51 -0500

    PHP - POO - Encapsulación

--- 02.php
+++ 02.php
@@ -3,37 +3,39 @@ declare( strict_types = 1);
 
 include 'includes/header.php';
 
-// ENCAPSULACIÓN
+// ? ENCAPSULACIÓN
 
 class Producto {
-
-    // Public - Se puede acceder y modificar en cualquier lugar (clase y objeto)
-    // protected - Se puede acceder / modificar unicamente en la clase
-    // private solo miembros de la misma clase pueden acceder a el
+    // ? Public - Se puede acceder y modificar en cualquier lugar (clase y objeto)
+    // ? protected - Se puede acceder / modificar unicamente en la clase
+    // ? private solo miembros de la misma clase pueden acceder a el
 
     public function __construct(protected string $nombre, public int $precio, public bool $disponible)
     {
     }
 
+    // ? void --> metodo vacio significa que no retorna nada
     public function mostrarProducto() : void {
         echo "El Producto es: " . $this->nombre . " y su precio es de: " . $this->precio;
     }
 
+    // ? Getters and Setters
+
+    // * retorna un string
     public function getNombre() : string {
         return $this->nombre;
     }
 
+    // * recibe un string
     public function setNombre(string $nombre) {
         $this->nombre = $nombre;
     }
-
 }
 
 $producto = new Producto('Tablet', 200, true);
-// $producto->mostrarProducto();
 
 echo $producto->getNombre();
-$producto->setNombre('Nuevo Nombre');
+$producto->setNombre('Portatil');
 
 echo "<pre>";
 var_dump($producto);
@@ -43,9 +45,5 @@ $producto2 = new Producto('Monitor Curvo', 300, true);
 // $producto2->mostrarProducto();
 
 echo $producto2->getNombre();
-
-// echo "<pre>";
-// var_dump($producto2);
-// echo "</pre>";
 
 include 'includes/footer.php';
